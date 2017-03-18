8501dc1c66b78cebb10bc3d645764b1650892c1a
Tomasz Kluczkowski <tomaszk1@hotmail.co.uk>
2017-03-18 09:33:21 +0000

    small change in validate pin

--- CODEWARS EXERCISES/validate_pin.py
+++ CODEWARS EXERCISES/validate_pin.py
@@ -2,7 +2,7 @@ def validate_pin(pin):
     import string
     if pin not in string.digits:
         return False
-    elif (len(pin) != 4 and len(pin) != 6):
+    elif len(pin) != 4 and len(pin) != 6:
         return False
     else:
         return True
