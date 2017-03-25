8303287e3ae3591b30ce4326562733992c117b1c
Tomasz Kluczkowski <tomaszk1@hotmail.co.uk>
2017-03-25 21:06:29 +0000

    udemy first round of lectures

---
+++ EDUCATION/UDEMY.COM/THE PYTHON MEGA COURSE/for loop iteration over multiple lists.py
@@ -0,0 +1,5 @@
+names = ["Tom", "Jon", "Ali"]
+email_domain = ["gmail", "hotmail", "yahoo"]
+
+for i,j in zip(names,email_domain):
+    print(i,j)
---
+++ EDUCATION/UDEMY.COM/THE PYTHON MEGA COURSE/mtfirstprog.py
@@ -0,0 +1,2 @@
+greeting = input("Please write greeting: ")
+print(greeting)
