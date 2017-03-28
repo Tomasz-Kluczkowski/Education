f67882d62b4f774e970379430d1cecdb413afd72
Tomasz Kluczkowski <tomaszk1@hotmail.co.uk>
2017-03-28 18:50:30 +0100

    finished card class, started turtleGTX exercise

--- EDUCATION/THINK LIKE A COMPUTER SCIENTIST FOR PYTHON 3/CHAPTER 22 COLLECTIONS OF OBJECTS/Card Class.py
+++ EDUCATION/THINK LIKE A COMPUTER SCIENTIST FOR PYTHON 3/CHAPTER 22 COLLECTIONS OF OBJECTS/Card Class.py
@@ -71,15 +71,112 @@ class Deck:
     def is_empty(self):
         return self.cards == []
 
+    def deal(self, hands, num_cards=999):
+        num_hands = len(hands)
+        for i in range(num_cards):
+            if self.is_empty():
+                break
+            card = self.pop()
+            hand = hands[i % num_hands]
+            hand.add(card)
 
 
-red_deck = Deck()
-blue_deck = Deck()
+class Hand(Deck):
+    def __init__(self, name=""):
+        self.cards = []
+        self.name = name
+
+    def add(self, card):
+        self.cards.append(card)
+
+    def __str__(self):
+        s = "Hand" + self.name
+        if self.is_empty():
+            s += " is empty\n"
+        else:
+            s += " contains\n"
+        return s + Deck.__str__(self)
+
+class CardGame:
+    def __init__(self):
+        self.deck = Deck()
+        self.deck.shuffle()
+
+class OldMaidHand(Hand):
+
+    def remove_matches(self):
+        count = 0
+        original_cards = self.cards[:]
+        for card in original_cards:
+            match = Card(3 - card.suit, card.rank)
+            if match in self.cards:
+                self.cards.remove(card)
+                self.cards.remove(match)
+                print("Hand {0}: {1} matches {2}".format(self.name, card, match))
+                count += 1
+        return count
+
+class OldMaidGame(CardGame):
+
+    def print_hands(self):
+        for hand in self.hands:
+            print(hand)
 
 
-print(red_deck)
 
-card1 = Card(1,1)
-card2 = Card(1, 13)
-print(card1 > card2)
-print(card1)
+    def find_neighbour(self, i):
+        num_hands = len(self.hands)
+        for next in range(1, num_hands):
+            neighbour = (i + next) % num_hands
+            if not self.hands[neighbour].is_empty():
+                return neighbour
+
+
+    def remove_all_matches(self):
+        count = 0
+        for hand in self.hands:
+            count += hand.remove_matches()
+        return count
+
+    def play_one_turn(self, i):
+        if self.hands[i].is_empty():
+            return 0
+        neighbour = self.find_neighbour(i)
+        picked_card = self.hands[neighbour].pop()
+        self.hands[i].add(picked_card)
+        print("Hand", self.hands[i].name, "picked", picked_card)
+        count = self.hands[i].remove_matches()
+        self.hands[i].shuffle()
+        return count
+
+
+    def play(self, names):
+        # remove queen of clubs
+        self.deck.remove(Card(0,12))
+
+        # make hand for each player
+        self.hands = []
+        for name in names:
+            self.hands.append(OldMaidHand(name))
+
+        # deal the cards
+        self.deck.deal(self.hands)
+        print("------------ Cards have been dealt")
+        self.print_hands()
+
+        # remove the initial matches
+        matches = self.remove_all_matches()
+        print("---------- Matches discarded, play begins")
+        self.print_hands()
+
+        # Play until all 50 cards are matched
+        turn = 0
+        num_hands = len(self.hands)
+        while matches < 25:
+            matches += self.play_one_turn(turn)
+            turn = (turn + 1) % num_hands
+        print("---------- Game is Over")
+        self.print_hands()
+
+game = OldMaidGame()
+game.play(["Tomek", "Wiola", "Natalka"])
