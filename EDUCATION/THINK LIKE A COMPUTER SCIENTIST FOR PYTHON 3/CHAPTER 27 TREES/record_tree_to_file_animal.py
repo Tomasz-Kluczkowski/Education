e6611adcd1c5b1a499bdd14b53459c84a27cdbba
Tomasz Kluczkowski <tomaszk1@hotmail.co.uk>
2017-04-06 23:32:04 +0100

    tree-list conversions fixed for animal guess game

--- EDUCATION/THINK LIKE A COMPUTER SCIENTIST FOR PYTHON 3/CHAPTER 27 TREES/record_tree_to_file_animal.py
+++ EDUCATION/THINK LIKE A COMPUTER SCIENTIST FOR PYTHON 3/CHAPTER 27 TREES/record_tree_to_file_animal.py
@@ -7,6 +7,7 @@ class Tree:
     def __str__(self):
         return str(self.cargo)
 
+
 def print_tree_indented(tree, level=0):
     if tree is None: return
     print_tree_indented(tree.right, level + 1)
@@ -29,19 +30,19 @@ def from_list_to_tree(cargo_list):
     bot_level = [root.left, root.right]
     while len(cargo_list) > 0:
         copy = bot_level[:]
-        for tree in copy:
-            # for trees which are None we don't add anything
-            if tree.cargo is None:
-                del bot_level[0]
-                continue
-            tree.left = Tree(cargo_list[0])
-            try:
-                tree.right = Tree(cargo_list[1])
-            except IndexError:
-                tree.right = Tree(None)
+        for t in copy:
+            if cargo_list[0] is None:
+                t.left = None
+            else:
+                t.left = Tree(cargo_list[0])
+
+            if cargo_list[1] is None:
+                t.right = None
+            else:
+                t.right = Tree(cargo_list[1])
             del cargo_list[0:2]
-            bot_level.append(tree.left)
-            bot_level.append(tree.right)
+            bot_level.append(t.left)
+            bot_level.append(t.right)
             del bot_level[0]
     return root
 
@@ -50,12 +51,9 @@ def from_tree_to_list(tree):
     # takes tree as an argument and returns a list of values corresponding to the cargos in each branch
     bot_level = [tree]
     cargo_list = []
-    # try:
-        # TODO try inserting a while loop condition as len(bot_level) > 0 and remove try / exepct clause
     while len(bot_level) > 0:
         copy = bot_level[:]
         for t in copy:
-            print(type(t))
             if isinstance(t, Tree):
                 cargo_list.append(t.cargo)
                 bot_level.append(t.left)
@@ -64,15 +62,15 @@ def from_tree_to_list(tree):
             else:
                 cargo_list.append(None)
                 del bot_level[0]
-    # except AttributeError:
-    #     pass
     return cargo_list
+
 
 def tree_to_file(tree, filename):
     with open(filename, "a") as file:
         cargo_list = from_tree_to_list(tree)
         for item in cargo_list:
-            file.write(str(item)+"\n")
+            file.write(str(item) + "\n")
+
 
 def file_to_tree(file):
     with open(file, "r") as file:
@@ -81,7 +79,7 @@ def file_to_tree(file):
         print(cargo_list)
     return from_list_to_tree(cargo_list)
 
-
+# TESTS FO THIS MODULE:
 # # [1, 2,3, 4,5,6,7, 8,9,10,11,12,13,14,15]
 #
 # cargo_list = [1, 2, None, 3, 4, 5,6,7,None,8,9,10,11,None,12]
@@ -99,9 +97,9 @@ def file_to_tree(file):
 
 # tree = from_list_to_tree(['can it bark', 'can it sting', 'dog', 'bird', 'moskito'])
 #
-tree = Tree(1, Tree(2), Tree(3))
-cargo_list = from_tree_to_list(tree)
-print(cargo_list)
-
-new_tree = from_list_to_tree(cargo_list)
-print_tree_indented(new_tree)
+# tree = Tree(1, Tree(2), Tree(3))
+# cargo_list = from_tree_to_list(tree)
+# print(cargo_list)
+#
+# new_tree = from_list_to_tree(cargo_list)
+# print_tree_indented(new_tree)
