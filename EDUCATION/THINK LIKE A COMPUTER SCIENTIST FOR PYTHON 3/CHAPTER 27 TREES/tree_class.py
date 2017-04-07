0831e9dfb4fabb7d4742988e6230a7c606eb9245
Tomasz Kluczkowski <tomaszk1@hotmail.co.uk>
2017-04-07 23:17:51 +0100

    updated Tree class with methods

--- EDUCATION/THINK LIKE A COMPUTER SCIENTIST FOR PYTHON 3/CHAPTER 27 TREES/tree_class.py
+++ EDUCATION/THINK LIKE A COMPUTER SCIENTIST FOR PYTHON 3/CHAPTER 27 TREES/tree_class.py
@@ -8,12 +8,43 @@ class Tree:
         return str(self.cargo)
 
     def print_tree_indented(self, level=0):
-        if self == None: return
-        if self.right == None: return
-        self.left.print_tree_indented(level + 1)
+        """Prints tree indented so that branches are visible"""
+        if self.left is not None:
+            self.left.print_tree_indented(level + 1)
         print("  " * level + str(self.cargo))
-        if self.right == None: return
-        self.right.print_tree_indented(level + 1)
+        if self.right is not None:
+            self.right.print_tree_indented(level + 1)
+
+    def print_tree_preorder(self):
+        """Prints tree in pre-order (pre-fix) """
+        print(self.cargo, end=" ")
+        if self.left is not None:
+            self.left.print_tree_preorder()
+        if self.right is not None:
+            self.right.print_tree_preorder()
+
+    def print_tree_postorder(self):
+        """Prints tree in post-order (post-fix) """
+        if self.left is not None:
+            self.left.print_tree_postorder()
+        if self.right is not None:
+            self.right.print_tree_postorder()
+        print(self.cargo, end=" ")
+
+    def print_tree_inorder(self):
+        """Prints tree in in-order (in-fix) """
+        if self.left is not None:
+            self.left.print_tree_inorder()
+        print(self.cargo, end=" ")
+        if self.right is not None:
+            self.right.print_tree_inorder()
+
 
 tree = Tree(1, Tree(2), Tree(3))
+
 tree.print_tree_indented()
+tree.print_tree_preorder()
+print()
+tree.print_tree_postorder()
+print()
+tree.print_tree_inorder()
