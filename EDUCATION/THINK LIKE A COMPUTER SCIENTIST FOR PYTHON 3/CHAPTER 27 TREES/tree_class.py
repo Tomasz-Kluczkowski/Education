979f1f66b2373eadc599c9b470b81de501d2ac25
Tomasz Kluczkowski <tomaszk1@hotmail.co.uk>
2017-04-12 23:04:39 +0100

    tree class

--- EDUCATION/THINK LIKE A COMPUTER SCIENTIST FOR PYTHON 3/CHAPTER 27 TREES/tree_class.py
+++ EDUCATION/THINK LIKE A COMPUTER SCIENTIST FOR PYTHON 3/CHAPTER 27 TREES/tree_class.py
@@ -8,12 +8,13 @@ class Tree:
         return str(self.cargo)
 
     def print_tree_indented(self, level=0):
-        """Prints tree indented so that branches are visible"""
-        if self.left is not None:
-            self.left.print_tree_indented(level + 1)
-        print("  " * level + str(self.cargo))
+        """Prints tree indented so that branches are visible since we print from top to bottom we have to print right
+        branch first to visualise left branch properly. """
         if self.right is not None:
             self.right.print_tree_indented(level + 1)
+        print("  " * level + str(self.cargo))
+        if self.left is not None:
+            self.left.print_tree_indented(level + 1)
 
     def print_tree_preorder(self):
         """Prints tree in pre-order (pre-fix) """
@@ -39,12 +40,28 @@ class Tree:
         if self.right is not None:
             self.right.print_tree_inorder()
 
+    # TODO: implement class method from tree to list
+    def from_tree_to_list(self):
+        """ Takes tree as an argument and returns a list of values corresponding to the cargos in each branch """
+        bot_level = [self]
+        cargo_list = []
+        while len(bot_level) > 0:
+            copy = bot_level[:]
+            for t in copy:
+                if isinstance(t, Tree):
+                    cargo_list.append(t.cargo)
+                    bot_level.append(t.left)
+                    bot_level.append(t.right)
+                    del bot_level[0]
+                else:
+                    cargo_list.append(None)
+                    del bot_level[0]
+        return cargo_list
 
-        # TODO: convert method below to classmethod for generating trees from lists
-        #add None to the tree if missing elements in the list
 
     @classmethod
     def from_list_to_tree(cls, cargo_list):
+    """Takes a list as argument and returns Tree object"""
         args = []
         try:
             for i in range(1, 3):
@@ -61,31 +78,37 @@ class Tree:
         while len(cargo_list) > 0:
             copy = bot_level[:]
             for t in copy:
-                if cargo_list[0] == "None":
+                try:
+                    if cargo_list[0] == "None":
+                        t.left = None
+                    else:
+                        t.left = Tree(cargo_list[0])
+                        bot_level.append(t.left)
+                except IndexError:
                     t.left = None
-                else:
-                    t.left = Tree(cargo_list[0])
-                    bot_level.append(t.left)
+                try:
+                    if cargo_list[1] == "None":
+                        t.right = None
+                    else:
 
-                if cargo_list[1] == "None":
+                        t.right = Tree(cargo_list[1])
+                        bot_level.append(t.right)
+                except IndexError:
                     t.right = None
-                else:
-                    t.right = Tree(cargo_list[1])
-                    bot_level.append(t.right)
                 del cargo_list[0:2]
                 del bot_level[0]
         return cls(root.cargo, root.left, root.right)
 
 
-tree = Tree(1, Tree(2), Tree(3))
+# tree = Tree(1, Tree(2), Tree(3))
+#
+# tree.print_tree_indented()
+# tree.print_tree_preorder()
+# print()
+# tree.print_tree_postorder()
+# print()
+# tree.print_tree_inorder()
 
-tree.print_tree_indented()
-tree.print_tree_preorder()
-print()
-tree.print_tree_postorder()
-print()
-tree.print_tree_inorder()
-
-cargo_list = [1, 2, 3, 4, 5, 6, 7]
+cargo_list = [1, 2, 3, 4]
 list_tree = Tree.from_list_to_tree(cargo_list)
 list_tree.print_tree_indented()
