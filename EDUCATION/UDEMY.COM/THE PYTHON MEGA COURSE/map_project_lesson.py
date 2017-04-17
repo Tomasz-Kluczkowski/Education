7e1253cbb1e6276f19552612d8b97017923f7c07
Tomasz Kluczkowski <tomaszk1@hotmail.co.uk>
2017-04-17 09:51:35 +0100

    map lesson finished

--- EDUCATION/UDEMY.COM/THE PYTHON MEGA COURSE/map_project_lesson.py
+++ EDUCATION/UDEMY.COM/THE PYTHON MEGA COURSE/map_project_lesson.py
@@ -16,12 +16,17 @@ def color(elev):
         col = 'red'
     return col
 
+fg = folium.FeatureGroup(name='Volcano Locations')
 
 for lat, lon, name, elev in zip(df["LAT"], df["LON"], df["NAME"], df["ELEV"]):
-    folium.Marker(location=[lat, lon], popup=name, icon=folium.Icon(icon="cloud", color=color(elev), icon_color='green')).add_to(my_map)
+    folium.Marker(location=[lat, lon], popup=name, icon=folium.Icon(icon="cloud", color=color(elev), icon_color='green')).add_to(fg)
+
+my_map.add_child(fg)
 
 my_map.add_child(folium.GeoJson(data=open('world-population.geo.json.txt'), name='World Population 2005',
-                                style_function=lambda x: {'fillcolor': 'green' if int(x['properties']['POP2005']) <= 10000000
+                                style_function=lambda x: {'fillColor': 'green' if int(x['properties']['POP2005']) <= 10000000
                                 else 'orange' if 10000000 < int(x['properties']['POP2005']) <= 20000000
                                 else 'red'}))
+my_map.add_child(folium.LayerControl())
+
 my_map.save("test_map.html")
